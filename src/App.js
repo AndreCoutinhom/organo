1184bc676394f6a2c3f08c030ad34a3bf523c8a6
André Coutinho <andre.coutinhom@gmail.com>
2023-11-24 02:03:10 -0300

    Campos de texto implantados

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import Banner from './components/Banner/Banner';
+ import Banner from './components/Banner';
+import CampoTexto from './components/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto label="Nome"/>
+      <CampoTexto label="Cargo"/>
+      <CampoTexto label="Imagem"/>
+      <CampoTexto label="Time"/>
+
     </div>
   );
 }
