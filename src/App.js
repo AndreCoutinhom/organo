6c02f9ed6b183eddf8aaa49392d98e64999b6c68
André Coutinho <andre.coutinhom@gmail.com>
2023-11-23 23:41:14 -0300

    Banner implantado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Olá. Boas vindas ao React!!
+          Boas vindas ao React!!
         </p>
         <a
           className="App-link"
