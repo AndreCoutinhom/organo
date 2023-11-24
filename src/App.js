96d8d643f00c45817c2914bd325e8e12d734d307
André Coutinho <andre.coutinhom@gmail.com>
2023-11-24 02:02:45 -0300

    Campos de texto implantados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './components/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Boas vindas ao React!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprenda a usar o React
-        </a>
-      </header>
     </div>
   );
 }
