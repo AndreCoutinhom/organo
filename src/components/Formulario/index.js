aba32257ac277445a35d23e213c1d4c2302e0af2
André Coutinho <andre.coutinhom@gmail.com>
2023-11-25 19:37:58 -0300

    Props do Formulario implantadas

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,14 +1,37 @@
+import Botao from '../Botao';
 import CampoTexto from '../CampoTexto';
+import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Inteligência Artificial',
+        'Devops',
+        'Ux & Design',
+        'Mobile',
+        'Inovação & Gestão'
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        console.log('Form foi submetido');
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="o seu nome"/>
-                <CampoTexto label="Cargo" placeholder="o seu cargo"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="o seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="o seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="o seu endereço de imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>
+                    Criar Card
+                </Botao>
 
             </form>
         </section>
